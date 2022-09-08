59342ab91df2964b8e8fc25b4a80568fd52d6ae8
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-08 19:56:54 +0300

    *Login page implemented

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router";
+function Login() {
+  let navigate = useNavigate();
+  function handleClick() {
+    navigate("/home");
+  }
+  return (
+    <div>
+      <button onClick={handleClick}>go home</button>
+    </div>
+  );
+}
+
+export default Login;
