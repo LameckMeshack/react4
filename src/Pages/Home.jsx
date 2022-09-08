4c9724a3c6f89664b836c6f99cf35e8d2b70e5a5
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-08 20:15:49 +0300

    *Added  button and UseNav hook

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import MyApp from "../components/context/MyApp";
-import Deffered from "../components/Deffered";
 import Form from "../components/form";
 import Layout from "../components/Layout";
-import Todos from "../components/Todos";
 import Users from "../components/User";
 
 export default function Home() {
+  const navigate = useNavigate();
+  const goTodos = () => {
+    navigate("/todos");
+  };
   return (
     <div>
       <h1>This is the home page</h1>
       <Form />
       <MyApp />
       <br />
-      <Todos />
+      <button onClick={goTodos}>Todo</button>
       <Users />
       <Layout />
-      <Deffered />
     </div>
   );
 }
