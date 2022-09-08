cf30e7185b9306852f8afe761b1786eef9b1c4f2
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-08 20:15:04 +0300

    *Added alert box in login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router";
 function Login() {
   let navigate = useNavigate();
   function handleClick() {
+    alert("You have successfully logged in");
     navigate("/home");
   }
   return (
