d409a5a224809d1f1762f0bd110479c7b127a1d0
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-08 20:30:33 +0300

    *Removed app from home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
-import MyApp from "../components/context/MyApp";
 import Form from "../components/form";
 import Layout from "../components/Layout";
 import Users from "../components/User";
@@ -15,7 +13,7 @@ export default function Home() {
     <div>
       <h1>This is the home page</h1>
       <Form />
-      <MyApp />
+
       <br />
       <button onClick={goTodos}>Todo</button>
       <Users />
