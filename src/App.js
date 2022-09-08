3e1ee527364887917443fd2d822cc4171d714460
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-08 20:17:04 +0300

    Refactored  by adding seearch and  todo routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
+import TodoPage from "./Pages/TodoPage";
+import Deffered from "./components/Deffered";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
+        <Route path="/todos" element={<TodoPage />} />
+        <Route path="/search" element={<Deffered />} />
       </Routes>
     </BrowserRouter>
   );
