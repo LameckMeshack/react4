2714019ead5719383ad4a7293e79cceb614ecaae
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-08 16:20:47 +0300

    *Created todo file

---
+++ src/components/Todos.jsx
@@ -0,0 +1,77 @@
+import { useReducer } from "react";
+
+function createInitialState(username) {
+  const initialTodos = [];
+  for (let i = 0; i < 5; i++) {
+    initialTodos.push({
+      id: i,
+      text: username + "'s task #" + (i + 1),
+    });
+  }
+  return {
+    draft: "",
+    todos: initialTodos,
+  };
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "changed_draft": {
+      return {
+        draft: action.nextDraft,
+        todos: state.todos,
+      };
+    }
+    case "added_todo": {
+      return {
+        draft: "",
+        todos: [
+          {
+            id: state.todos.length,
+            text: state.draft,
+          },
+          ...state.todos,
+        ],
+      };
+    }
+    case "deleted_todo": {
+      return {
+        draft: state.draft,
+        todos: state.todos.filter((todo) => todo.id !== action.id),
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+  //   throw Error("Unknown action: " + action.type);
+}
+
+export default function Todos({ username = "Guest" }) {
+  const [state, dispatch] = useReducer(reducer, username, createInitialState);
+  return (
+    <>
+      <input
+        value={state.draft}
+        onChange={(e) => {
+          dispatch({
+            type: "changed_draft",
+            nextDraft: e.target.value,
+          });
+        }}
+      />
+      <button
+        onClick={() => {
+          dispatch({ type: "added_todo" });
+        }}
+      >
+        Add
+      </button>
+      <ul>
+        {state.todos.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
