9db46990688cfba6929d1d1071943a7cbd30f4ba
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-09 08:49:31 +0300

    *Added route for single Todo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import TodoPage from "./Pages/TodoPage";
 import Deffered from "./components/Deffered";
+import Todo from "./components/Todo";
 
 function App() {
   return (
@@ -13,8 +14,9 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
-        <Route path="/todos" element={<TodoPage />} />
-        <Route path="/search" element={<Deffered />} />
+        <Route path="todos" element={<TodoPage />} />
+        <Route path="search" element={<Deffered />} />
+        <Route path="todos/:id" element={<Todo />} />
       </Routes>
     </BrowserRouter>
   );
